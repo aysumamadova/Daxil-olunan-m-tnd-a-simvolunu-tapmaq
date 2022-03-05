02886973060a1a9e8a0ce2741d2a5499f0dace89
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-05 11:43:55 +0400

    first

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Soz daxil edin : ");
+            string soz = Convert.ToString(Console.ReadLine());
+            char herf = 'a';
+            int sum = 0;
+
+            for (int i = 0; i < soz.Length; i++)
+            {
+                if (soz[i] == herf)
+                {
+                    sum++;
+                }
+
+            }
+            if (sum > 0)
+            {
+                Console.WriteLine("a herfi var");
+            }
+            else
+            {
+                Console.WriteLine("a herfi yoxdu");
+            }
+        }
+    }
+}
